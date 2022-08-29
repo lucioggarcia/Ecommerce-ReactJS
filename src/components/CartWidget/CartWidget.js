9064a76813e395468067cea54057537a85528908
Lucio Garcia <garcialuciogonzalo@gmail.com>
2022-08-29 20:30:49 -0300

    cartwidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,7 +2,8 @@
 const CartWidget= ()=>{
     return(
         <div className="cartWidget_Container">
-            <button className='cartWidget_button'>cart</button>
+            <button className='cartWidget_button'><i class="fas fa-shopping-cart"></i></button>
+            
         </div>
     )
 }
