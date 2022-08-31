b3209c08787be2c289dc9296d4e70c38ee4013f9
Lucio Garcia <garcialuciogonzalo@gmail.com>
2022-08-31 20:15:27 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import NavBar from '../src/components/NavBar/NavBar.js';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <NavBar />
         <ItemListContainer greeting='Hola, esto es la entrega de un desafio' />
-       
+        <ItemCount stock={10} initial={0} />
       </header>
     </div>
   );
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+function ItemCount({stock, initial}){
+    const [count,setCount]=useState(initial)
+    return(
+        <div>
+            <h3>Remera Verde, Cantidad: {count}</h3>
+            <button onClick={()=>setCount(count+1)} disabled={count===stock}>+</button>
+            <button onClick={()=>setCount(count-1)} disabled={count===0}>+</button>
+        </div>
+    )
+}
+export default ItemCount;
